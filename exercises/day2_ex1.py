9fc6bf6426e2cae0c340d9f616410cdd09eabb6f
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-02-11 05:12:27 +0530

    add day2 first exercise

---
+++ exercises/day2_ex1.py
@@ -0,0 +1,6 @@
+"""
+Create a program that prompts the user to input their name once. Then, the program prints out the name once with the first letter capitalized.
+"""
+
+name = input("Enter Your Name:")
+print(name.capitalize())
