d97a3f34274b2e323cdb13ca3493d4abf66879d7
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-02-10 13:37:25 +0530

    delete bonusExercise

--- bonusExercise1.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to Letter Calculator")
-
-# taking user input
-user_prompt = "Write the Tittle: "
-title = input(user_prompt)
-
-# calculating the length and printing it out
-title_len = len(title)
-print("Your title length is", title_len)
