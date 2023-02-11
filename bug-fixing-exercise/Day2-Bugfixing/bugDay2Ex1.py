fa9c8e508ba9a011ce4f696da21d9915784abe3f
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-02-11 05:40:15 +0530

    add bug fixing exercises of day2

---
+++ bug-fixing-exercise/Day2-Bugfixing/bugDay2Ex1.py
@@ -0,0 +1,10 @@
+"""
+The code below has two bugs. Hunt them down and fix them.
+    while True
+    print("Hello")
+"""
+
+# answer : ":" is missing and in the next line indention error.
+
+while True:
+    print("Hello")
