623ce0d7166ad508f9d348e7f9d2dc2d68d8d029
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-02-12 04:56:54 +0530

    add exit feature to the todo app

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ todo_list = []
 
 # Taking User Input:
 while True:
-    user_input = input("Type add or show")
+    user_input = input("Type add, show or exit: ")
 
+    # matching user input with the assigned cases:
     match user_input:
+
         case "add":
             todo = input("Enter a todo: ")
             todo_list.append(todo)
+
         case "show":
             print(todo_list)
+
+        case "exit":
+            break
+
+print("Thank you for using Todo App")
