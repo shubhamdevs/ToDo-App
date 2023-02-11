48ca76a3603ed03d5ec0862bcd84288aecdf4913
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-02-12 04:43:38 +0530

    add feature to show todos

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 print("Welcome to ToDo App")
 
-user_prompt = "Enter a todo: "
-todos = []
+todo_list = []
 
 # Taking User Input:
 while True:
-    text = input(user_prompt)
-    todos.append(text)
-    print(todos)
+    user_input = input("Type add or show")
+
+    match user_input:
+        case "add":
+            todo = input("Enter a todo: ")
+            todo_list.append(todo)
+        case "show":
+            print(todo_list)
