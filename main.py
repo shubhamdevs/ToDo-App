8784b31bc880d24168539a0c7ead6e8a7ba54a07
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-02-15 06:11:56 +0530

    add feature to store todos in a file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 print("Welcome to ToDo App")
 
-todo_list = []
-
 # Taking User Input:
 while True:
     user_input = input("Type add, edit, complete, show or exit: ")
@@ -11,21 +9,41 @@ while True:
     match user_input:
 
         case "add":
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open("todos.txt", "r")
+            todo_list = file.readlines()
+            file.close()
+
             todo_list.append(todo)
 
+            file = open("todos.txt", "w")
+            file.writelines(todo_list)
+            file.close()
+
         case "edit":
+            file = open("todos.txt", "r")
+            todo_list = file.readlines()
             num = int(input("Number of the todo to edit: "))
             new_todo = input("Enter the new todo: ")
+            file = open("todos.txt", "w")
             todo_list[num - 1] = new_todo
+            file.close()
 
         case "complete":
+            file = open("todos.txt", "w")
+            todo_list = file.readlines()
             num = int(input("Enter the number of completed todo: "))
             todo_list.pop(num - 1)
+            file.close()
 
         case "show":
+            file = open("todos.txt", "r")
+            todo_list = file.readlines()
             for index, item in enumerate(todo_list):
                 print(f"{index + 1}.{item}")
+
+            file.close()
 
         case "exit":
             break
