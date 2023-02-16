fdc4141bf90078870c5206ca7267b5631d8fa050
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-02-16 07:04:10 +0530

    update app code for better readability

--- main.py
+++ main.py
@@ -9,26 +9,25 @@ while True:
     match user_input:
 
         case "add":
+            with open("todos.txt", "r") as file:
+                todo_list = file.readlines()
+
             todo = input("Enter a todo: ") + "\n"
-
-            file = open("todos.txt", "r")
-            todo_list = file.readlines()
-            file.close()
-
             todo_list.append(todo)
 
-            file = open("todos.txt", "w")
-            file.writelines(todo_list)
-            file.close()
+            with open("todos.txt", "w") as file:
+                file.writelines(todo_list)
 
         case "edit":
+            with open("todos.txt", "r") as file:
+                todo_list = file.readlines()
 
-            file = open("todos.txt", "r")
-            todo_list = file.readlines()
             num = int(input("Number of the todo to edit: "))
             new_todo = input("Enter the new todo: ")
             todo_list[num - 1] = new_todo
-            file.close()
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todo_list)
 
         case "complete":
             file = open("todos.txt", "w")
