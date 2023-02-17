f144a2d8da943e1e4aecc69aa4eba47a83f28d50
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-02-17 06:35:35 +0530

    fix bug in complete feature for index error

--- main.py
+++ main.py
@@ -29,24 +29,30 @@ while True:
             with open("todos.txt", "w") as file:
                 file.writelines(todo_list)
 
-        # Checking for a Value Error as takes integers.
+        # Checking for a Value Error as takes integers and if the user only mention strings.
         except ValueError:
             print("Your Command is not valid")
             continue
 
     elif user_input.startswith("complete"):
-        with open("todos.txt", "r") as file:
-            todo_list = file.readlines()
+        try:
+            with open("todos.txt", "r") as file:
+                todo_list = file.readlines()
 
-        num = int(user_input[9:])
-        index = num - 1
-        todoToDelete = todo_list[index].strip("\n")
-        todo_list.pop(index)
+            num = int(user_input[9:])
+            index = num - 1
+            todoToDelete = todo_list[index].strip("\n")
+            todo_list.pop(index)
 
-        with open("todos.txt", "w") as file:
-            file.writelines(todo_list)
+            with open("todos.txt", "w") as file:
+                file.writelines(todo_list)
 
-        print(f"Task {todoToDelete} is removed from the list")
+            print(f"Task {todoToDelete} is removed from the list")
+
+        # Checking if the user input a task number that is not present in the task list. It will give Index Error.
+        except IndexError:
+            print("There is no item with that number: ")
+            continue
 
     elif user_input.startswith("show"):
         with open("todos.txt", "r") as file:
