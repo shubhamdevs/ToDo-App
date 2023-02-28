a337f65d43c0165b3cf135b8094b3c479435befd
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-03-01 02:04:47 +0530

    add function to write todolist file

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ print("Welcome to ToDo App")
 
 
 # Crating a get_todos() function to get previous tasks.
-def get_todos():
-    with open("todos.txt", "r") as file_local:
+def get_todos(filepath):
+    with open(filepath, "r") as file_local:
         todo_list_local = file_local.readlines()
     return todo_list_local
+
+
+def write_todos(filepath, todo_arg):
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todo_arg)
 
 
 # Taking User Input:
@@ -15,24 +20,22 @@ while True:
 
     # matching user input with the assigned cases:
     if user_input.startswith("add"):
-        todo_list = get_todos()
+        todo_list = get_todos("todos.txt")
         todo = user_input[4:]
         todo_list.append(todo + "\n")
 
-        with open("todos.txt", "w") as file:
-            file.writelines(todo_list)
+        write_todos("todos.txt", todo_list)
 
     elif user_input.startswith("edit"):
         try:
-            todo_list = get_todos()
+            todo_list = get_todos("todos.txt")
 
             num = int(user_input[5:])
             new_todo = input("Enter the new todo: ")
 
             todo_list[num - 1] = new_todo + "\n"
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todo_list)
+            write_todos("todos.txt", todo_list)
 
         # Checking for a Value Error as takes integers and if the user only mention strings.
         except ValueError:
@@ -41,15 +44,14 @@ while True:
 
     elif user_input.startswith("complete"):
         try:
-            todo_list = get_todos()
+            todo_list = get_todos("todos.txt")
 
             num = int(user_input[9:])
             index = num - 1
             todoToDelete = todo_list[index].strip("\n")
             todo_list.pop(index)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todo_list)
+            write_todos("todos.txt", todo_list)
 
             print(f"Task {todoToDelete} is removed from the list")
 
@@ -59,7 +61,7 @@ while True:
             continue
 
     elif user_input.startswith("show"):
-        todo_list = get_todos()
+        todo_list = get_todos("todos.txt")
 
         for index, item in enumerate(todo_list):
             item = item.strip("\n")
