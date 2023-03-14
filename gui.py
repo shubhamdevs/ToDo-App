453c06397b4b24f9502b5bc414e8f9c8233ce7e9
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-03-14 12:08:29 +0530

    Implement Exit button in GUI App

--- gui.py
+++ gui.py
@@ -8,11 +8,13 @@ list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
                    layout=[[label],
                            [input_box, add_button],
-                           [list_box, edit_button, complete_button]],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -44,6 +46,9 @@ while True:
             functions.write_todos(todos)
             window["todos"].update(values=todos)
             window["todo"].update(value="")
+
+        case "Exit":
+            break
 
         case "todos":
             window["todo"].update(value=values["todos"][0])
