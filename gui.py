b99c43aae22df5834c2433c4b57fe6395300baf5
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-03-14 12:29:07 +0530

    change theme and size of edit button in GUI App

--- gui.py
+++ gui.py
@@ -2,10 +2,11 @@ import functions
 import PySimpleGUI as sg
 import time
 
+sg.theme("Black")
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", size=10)
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
@@ -23,7 +24,7 @@ window = sg.Window("My To-Do App",
 while True:
     event, values = window.read(timeout=200)
     window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
-    
+
     match event:
         case "Add":
             todos = functions.get_todos()
