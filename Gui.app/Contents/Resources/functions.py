f47107ba9d7e4bdb99d59d35cab1107bbecb00d4
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-03-15 10:34:50 +0530

    create a static web interface

---
+++ Gui.app/Contents/Resources/functions.py
@@ -0,0 +1,15 @@
+FILEPATH = "todos.txt"
+
+
+# reading the todo list and listing them in a variable
+def get_todos(filepath=FILEPATH):
+    with open(filepath, "r") as file_local:
+        todo_list_local = file_local.readlines()
+    return todo_list_local
+
+
+# writing the todo list with assigned values
+def write_todos(todo_arg, filepath=FILEPATH):
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todo_arg)
+ 
